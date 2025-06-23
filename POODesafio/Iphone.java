16a729666585bbe95510c5e9f5981f8459095697
mathuesssd <matheusa2021@gmail.com>
2025-06-23 08:39:07 -0300

    diagramação de um componente iphone

---
+++ POODesafio/Iphone.java
@@ -0,0 +1,59 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    //ReprodutorMusical
+    @Override
+    public void pausar() {
+       
+        
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+       
+        
+    }
+
+    @Override
+    public void tocar() {
+       
+        
+    }
+    
+    //AparelhoTelefonico
+    @Override
+    public void atender() {
+       
+        
+    }
+
+    @Override
+    public void inicarCorreioVoz() {
+       
+        
+    }
+
+    @Override
+    public void ligar(String numero) {
+       
+        
+    }
+    
+    //NavegadorInternet
+    @Override
+    public void adicionarNovaAba() {
+       
+        
+    }
+
+    @Override
+    public void atualizarPagina() {
+       
+        
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+       
+        
+    }
+
+}
---
+++ POODesafio/Readme.md
@@ -0,0 +1,4 @@
+# Entrega do Desafio
+Modelagem e Diagramação de um Componente iPhone
+
+![Diagrama de classes](./iphone.drawio.png)
